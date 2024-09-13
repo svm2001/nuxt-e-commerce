f83b2254ef773e17616b8d3643ea20ff5f607ce1
Slava <sidorov@kompot.bz>
2024-09-14 02:25:49 +0400

    ..

--- components/admin/forms/edit/product.vue
+++ components/admin/forms/edit/product.vue
@@ -66,19 +66,15 @@ const uploadImages = async () => {
 const submitForm = async () => {
     try {
         productStore.loading = true;
-        const uploadedImages = await uploadImages(newImages.value);
-
-        const validImages = images.value.filter(img => typeof img === 'string' && img.startsWith('[http'))
+        // const uploadedImages = await uploadImages(newImages.value);
+        // const validImages = images.value.filter(img => typeof img === 'string' && img.startsWith('[http'))
 
         const updatedProduct = {
             title: title.value,
             price: price.value,
             description: description.value,
-            categoryId: categoryId.value,
-            images: validImages.concat(uploadedImages),
+            // images: validImages.concat(uploadedImages),
         };
-
-        console.log(JSON.stringify(updatedProduct.images))
 
         const response = await fetch(`https://api.escuelajs.co/api/v1/products/${props.product.id}`, {
             method: 'PUT',
@@ -136,60 +132,59 @@ const submitForm = async () => {
                     </FormItem>
                 </FormField>
 
-                <FormField v-slot="{ componentField }" name="categoryId">
-                    <FormItem>
-                        <FormControl>
-                            <Select v-model="categoryId">
-                                <SelectTrigger class="w-full">
-                                    <SelectValue placeholder="Select a category" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectGroup>
-                                        <SelectItem v-for="category in productStore.categories" :key="category.id" :value="category.id">
-                                            {{ category.name }}
-                                        </SelectItem>
-                                    </SelectGroup>
-                                </SelectContent>
-                            </Select>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-                {{props.product.images}}
+<!--                <FormField v-slot="{ componentField }" name="categoryId">-->
+<!--                    <FormItem>-->
+<!--                        <FormControl>-->
+<!--                            <Select v-model="categoryId">-->
+<!--                                <SelectTrigger class="w-full">-->
+<!--                                    <SelectValue placeholder="Select a category" />-->
+<!--                                </SelectTrigger>-->
+<!--                                <SelectContent>-->
+<!--                                    <SelectGroup>-->
+<!--                                        <SelectItem v-for="category in productStore.categories" :key="category.id" :value="category.id">-->
+<!--                                            {{ category.name }}-->
+<!--                                        </SelectItem>-->
+<!--                                    </SelectGroup>-->
+<!--                                </SelectContent>-->
+<!--                            </Select>-->
+<!--                        </FormControl>-->
+<!--                        <FormMessage />-->
+<!--                    </FormItem>-->
+<!--                </FormField>-->
 
-                <FormField v-slot="{ componentField }" name="categoryId">
-                    <FormItem>
-                        <FormControl>
-                            <div class="flex flex-col gap-4">
-                                <div class="">
-                                    <div class="file-input">
-                                        <input
-                                            id="file-input"
-                                            class="file-input__input"
-                                            type="file"
-                                            multiple
-                                            @change="handleImageUpload"
-                                            accept="image/*"
-                                        />
-                                        <label class="file-input__label" for="file-input">
-                                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="upload" class="svg-inline--fa fa-upload fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path></svg>
-                                            <span>Upload file(s)</span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="flex gap-[18px]">
-                                    <div v-for="(preview, index) in imagePreviews" :key="index" class="relative">
-                                        <img :src="preview" alt="Preview" class="w-20 h-20 object-cover rounded" />
-                                        <button @click="removeImage(index)" class="absolute top-0 right-0 bg-red-600 text-white rounded-full w-5 h-5 flex items-center justify-center">
-                                            <Icon name="carbon:close" class="w-4 h-4" />
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
+<!--                <FormField v-slot="{ componentField }" name="file">-->
+<!--                    <FormItem>-->
+<!--                        <FormControl>-->
+<!--                            <div class="flex flex-col gap-4">-->
+<!--                                <div class="">-->
+<!--                                    <div class="file-input">-->
+<!--                                        <input-->
+<!--                                            id="file-input"-->
+<!--                                            class="file-input__input"-->
+<!--                                            type="file"-->
+<!--                                            multiple-->
+<!--                                            @change="handleImageUpload"-->
+<!--                                            accept="image/*"-->
+<!--                                        />-->
+<!--                                        <label class="file-input__label" for="file-input">-->
+<!--                                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="upload" class="svg-inline&#45;&#45;fa fa-upload fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path></svg>-->
+<!--                                            <span>Upload file(s)</span>-->
+<!--                                        </label>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <div class="flex gap-[18px]">-->
+<!--                                    <div v-for="(preview, index) in imagePreviews" :key="index" class="relative">-->
+<!--                                        <img :src="preview" alt="Preview" class="w-20 h-20 object-cover rounded" />-->
+<!--                                        <button @click="removeImage(index)" class="absolute top-0 right-0 bg-red-600 text-white rounded-full w-5 h-5 flex items-center justify-center">-->
+<!--                                            <Icon name="carbon:close" class="w-4 h-4" />-->
+<!--                                        </button>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                        </FormControl>-->
+<!--                        <FormMessage />-->
+<!--                    </FormItem>-->
+<!--                </FormField>-->
                 <DialogClose class="hidden">
                     <button ref="close">CLOSE</button>
                 </DialogClose>
